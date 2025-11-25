d5feb98ab6787c1a7aea16b3ff912a1b1ed84cdd
dimto <dimto@online.de>
2025-11-25 23:46:09 +0100

    browse / msi: Erster erfolgreicher Lauf mit eigenem ollama Modell

--- main/run_browser_with_ollama.py
+++ main/run_browser_with_ollama.py
@@ -7,18 +7,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatOllama(
-    model=os.getenv("BROWSER_USE_LLM_MODEL", "deepseek-r1:32b"),  # gpt-oss:20b
+    model=os.getenv("BROWSER_USE_LLM_MODEL", "deepseek-r1:32b"),  # gpt-oss:20b    deepseek-r1:32b
     host=os.getenv("OLLAMA_ENDPOINT", "http://localhost:11434")
 )
 
 async def run_agent():
     agent = Agent(
         # task="find the founders of browser-use",
-        task="get latest price of xauusd from https://www.tradingview.com/",
+        # task="get latest price of xauusd. but also try to use different sources if 3 times the same source is used",
+        task="open this page: 'https://www.tradingview.com/chart/?symbol=ECONOMICS:FRGDPYY' and figure out if this link has finance information.",
         llm=llm,
-        max_actions_per_step=3
+        max_actions_per_step=5
     )
-    result = await agent.run(max_steps=10)
+    result = await agent.run(max_steps=15)
     print(result)
 
 if __name__ == "__main__":
